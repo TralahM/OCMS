f8613ead431286e1f101a61baa2604a3871517f2
TralahM <musyoki.tralah@students.jkuat.ac.ke>
2019-07-17 00:52:42 +0300

    update settings

--- complete/settings.py
+++ complete/settings.py
@@ -11,11 +11,13 @@ https://docs.djangoproject.com/en/2.1/ref/settings/
 """
 
 import os
+import django_heroku
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+PROJECT_ROOT = os.path.dirname(os.path.abspath(__file__))
 STATIC_DIR = os.path.join(BASE_DIR, 'static')
-STATIC_ROOT = STATIC_DIR
+STATIC_ROOT = os.path.join(PROJECT_ROOT, 'static')
 
 
 # Channels-specific settings
@@ -212,3 +214,4 @@ EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_HOST_USER = 'laxmanironman@gmail.com'
 EMAIL_HOST_PASSWORD = 'tommyjerry'
 EMAIL_PORT = 587
+django_heroku.settings(locals())
